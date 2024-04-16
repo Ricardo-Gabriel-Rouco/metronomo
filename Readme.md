0c3d1e4c392e00e0479b9513556a502efb7c3858
Ricardo-Gabriel-Rouco <r.g.rouco1@gmail.com>
2024-04-15 21:32:50 -0300

    feat: :art: agregado readme e imagen circuito

---
+++ Readme.md
@@ -0,0 +1,66 @@
+# Metronomo
+
+## Descripción
+
+Este repositorio contiene el código fuente para un proyecto de Arduino que simplemente es un metronomo realizado en una protoboard con el fin de practicar el uso de un shift register y poder emitir datos al usuario y mostrarlos al mismo.
+
+## Contenido del Repositorio
+
+- `metronomo.ino`: Archivo principal que contiene el código de Arduino.
+- `README.md`: Este archivo que proporciona información sobre el proyecto.
+
+## Requisitos
+
+Para cargar y ejecutar el código en tu placa Arduino, necesitarás lo siguiente:
+
+- Una placa Arduino compatible.
+- El IDE de Arduino instalado en tu computadora.
+- La libreria LiquidCrystal_I2C con el fin de controlar la pantalla lcd.
+
+## Materiales
+
+| Cantidad | Componente                                     |
+|----------|------------------------------------------------|
+| 1        | Arduino Uno R3                                 |
+| 3        | Pulsador                                       |
+| 1        | LCD 16 x 2 (I2C)*  |
+| 3        | Resistencia de 10 kΩ                           |
+| 1        | Buzzer pasivo                                  |
+| 1        | Shift register 74HC595N                        |
+| 3        | LED Rojo*                                      |
+| 3        | LED Amarillo*                                  |
+| 2        | LED Verde*                                     |
+| 8        | Resistencia de 220 Ω                           |
+
+Componentes opcionales:
+
+- LED Rojo, LED Amarillo, LED Verde: Se pueden usar de cualquier color, utilize estos ya que me venian en el kit.
+- LCD 16 x 2 (I2C): use con adapdator I2C ya que estos utilizan solo 4 cables en vez de 16 como los que no tienen este adaptador.
+
+## Instrucciones de Uso
+
+1. Abre el archivo `metronomo.ino` en el IDE de Arduino.
+2. Recorda descargar la libreria LiquidCrystal_I2C desde el gestor del arduino IDE.
+3. Conecta tu placa Arduino a tu computadora mediante un cable USB.
+4. Selecciona el tipo de placa y el puerto serie correctos en el IDE de Arduino.
+5. Haz clic en el botón "Cargar" para compilar y cargar el código en la placa Arduino.
+
+## Circuito
+
+<img src="./Brave Jaban-Robo.png" alt="Texto alternativo" width="700" height="300">
+
+## Contribuciones
+
+Si deseas contribuir a este proyecto, siéntete libre de hacerlo. Puedes enviar solicitudes de extracción (pull requests) para proponer cambios o mejoras.
+
+## Problemas
+
+Si encuentras algún problema o tienes alguna pregunta sobre este proyecto, por favor, abre un nuevo issue en este repositorio.
+
+## Licencia
+
+[Indica la licencia bajo la cual se distribuye el código, por ejemplo, MIT License].
+
+## Agradecimientos
+
+[Opcional: Agradecimientos a las personas o recursos que han contribuido al proyecto].
--- metronomo.ino
+++ metronomo.ino
@@ -1,45 +1,116 @@
+// importamos librerias
 #include <LiquidCrystal_I2C.h>
-#include <Wire.h>  // Se corrigió, se eliminó el ; al final de la definición de WIRE
+#include <Wire.h>
 
-int tempo = 60000;
-int BPM = 120;  // default para tempo promedio
-
+// iniciamos el lcd
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+// pines
 int pulsadorMas = 2;
-int pulsadorMenos = 4;
+int pulsadorMenos = 3;
+int pulsadorReset = 4;
+int latchPin = 8;
+int buzzer = 11;
+int dataPin = 12;
+int clockPin = 13;
 
-void setup() {
+// Variables const
+const int tempo = 60000;
+const int limiteSup = 300;
+const int limiteInf = 10;
+const int BPMdefault = 120;
+
+// variables iniciales
+int BPM = BPMdefault;
+byte myByte = 0b00000000; // Inicialmente todos los LEDs apagados
+float dt;
+
+// unsigned long ultimoTiempoMas = 0;
+// unsigned long ultimoTiempoMenos = 0;
+// unsigned long intervaloDebounce = 50; // Tiempo de debounce en milisegundos
+
+// funciones
+void actualizarRegistro(byte byteADesplazar)
+{
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, LSBFIRST, byteADesplazar);
+  digitalWrite(latchPin, HIGH);
+}
+
+void setup()
+{
   Serial.begin(9600);
-  pinMode(pulsadorMas, INPUT_PULLUP);
-  pinMode(pulsadorMenos, INPUT_PULLUP);
+  pinMode(pulsadorMas, INPUT);
+  pinMode(pulsadorMenos, INPUT);
+  pinMode(pulsadorReset, INPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(buzzer, OUTPUT);
   lcd.init();
   lcd.backlight();
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Tempo:");  // 6 caracteres
+  lcd.print("Tempo:");
 }
 
-void loop() {
+void loop()
+{
   int lecturamas = digitalRead(pulsadorMas);
   int lecturamenos = digitalRead(pulsadorMenos);
-  switch (lecturamas) {
-    case HIGH:
-      if (BPM <= 400) {
-        BPM = BPM + 10;
-      }
-      break;
-  }
-  switch (lecturamenos) {
-    case HIGH:
-      if (BPM > 10) {
-        BPM = BPM - 10;
-      }
-    break;
-  }
-
-
+  int lecturareset = digitalRead(pulsadorReset);
+  // && millis() - ultimoTiempoMas > intervaloDebounce
+  // && millis() - ultimoTiempoMenos > intervaloDebounce
   lcd.setCursor(7, 0);
-  lcd.print(BPM);
+  dt = ((tempo / BPM) / 4) * -1;
+
+  if (lecturamas == HIGH && BPM < limiteSup)
+  {
+    BPM = BPM + 10; // Aumenta BPM en 10 pero no supera el límite de 400
+  }
+  else if (lecturamas == HIGH && BPM == limiteSup)
+  {
+    tone(buzzer, 2500, 100);
+  }
+
+  if (lecturamenos == HIGH && BPM > limiteInf)
+  {
+    BPM = BPM - 1; // Disminuye BPM en 10 pero no baja del límite de 10
+  }
+  else if (lecturamenos == HIGH && BPM == limiteInf)
+  {
+    tone(buzzer, 2500, 100);
+  }
+
+  if (lecturareset == HIGH)
+  {
+    BPM = BPMdefault;
+    tone(buzzer, 1500, 100);
+  }
+  for (int i = 0; i < 8; i++)
+  {
+    myByte |= (1 << i); // Enciende el i-ésimo LED
+    actualizarRegistro(myByte);
+    delay(dt);
+    tone(buzzer, 1000, 20);
+  }
+
+  for (int i = 7; i >= 0; i--)
+  {
+    myByte &= ~(1 << i); // Apaga el i-ésimo LED
+    actualizarRegistro(myByte);
+    delay(dt);
+    tone(buzzer, 1000, 20);
+  }
+
+  if (BPM < 100)
+  {
+    lcd.print(0);
+    lcd.print(BPM);
+  }
+  else
+  {
+    lcd.print(BPM);
+  }
   delay(200);
 }
